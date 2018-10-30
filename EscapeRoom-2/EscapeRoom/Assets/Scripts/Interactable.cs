9fa94dbfefe46eb5746a308f02bc9e85572dc66b
ATchibo <44547421+ATchibo@users.noreply.github.com>
2018-10-30 16:43:28 +0200

    mic update(tedi vezi comm ul din playercontroller)

--- EscapeRoom-2/EscapeRoom/Assets/Scripts/Interactable.cs
+++ EscapeRoom-2/EscapeRoom/Assets/Scripts/Interactable.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactable : MonoBehaviour {
 
     public float radius = 3f;
 
+    public Sprite icon;
+
+    public GameObject obj;
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void setTexture(Image image)
+    {
+        image.sprite = icon;
+        print("Am setat");
+    }
+
+    public void popOut()
+    {
+        obj.SetActive(false);
+    }
+
+    public void spawn(Image image, Vector3 spawnCoords, Quaternion rotation)
+    {
+        Instantiate(obj, spawnCoords, rotation);
+        obj.SetActive(true);
+        image.sprite = null;
     }
 }
--- EscapeRoom-2/EscapeRoom/Assets/Scripts/PlayerController.cs
+++ EscapeRoom-2/EscapeRoom/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,13 @@ public class PlayerController : MonoBehaviour {
     Rigidbody rb;
     public Camera camera;
 
+    protected Joystick joystick;
+
     public Text interactableName;
+
+    public Image[] inventorySlots;
+
+    Interactable interactable;
 
     public float minDist;
     public float speed;
@@ -22,6 +28,7 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        joystick = FindObjectOfType <Joystick>();
 	}
 	
 	// Update is called once per frame
@@ -29,19 +36,31 @@ public class PlayerController : MonoBehaviour {
         playerMove();
         cameraMove();
         checkForHit();
+        tempSpawn();
     }
 
     void playerMove()
     {
-        //horizMov = Input.GetAxisRaw("Horizontal");
-        vertMov = Input.GetAxis("Vertical");
+<<<<<<< HEAD
+        /*rb.velocity = new Vector3((joystick.Horizontal * speed) + (Input.GetAxis("Horizontal") * speed),
+                                    rb.velocity.y, 
+                                   (joystick.Vertical * speed) + (Input.GetAxis("Vertical") * speed));*/ //pt telefon e buna asta, dar daca rotesti playerul la 180 de grade spre ex si apesi w se duce cu spatele
+        rb.velocity = transform.forward * speed * Input.GetAxis("Vertical");
+=======
 
-        rb.velocity = transform.forward * speed * vertMov;
+        rb.velocity = new Vector3((joystick.Horizontal * speed) + (Input.GetAxis("Horizontal") * speed),
+                                  rb.velocity.y, 
+                                  (joystick.Vertical * speed) + (Input.GetAxis("Vertical") * speed));
+>>>>>>> 96fe34625cda0bd51f1b696efd06a5a2e610af82
     }
 
     void cameraMove()
     {
-        if (Input.GetMouseButton(0))
+<<<<<<< HEAD
+        if (Input.GetButton("Fire1"))// && (Input.touchCount > 1))// <-Pentru Android se decomenteaza si se introduce sub acelasi if
+=======
+        if (Input.GetButton("Fire1")) //&& (Input.touchCount > 1)) <-Pentru Android se decomenteaza si se introduce sub acelasi if
+>>>>>>> 96fe34625cda0bd51f1b696efd06a5a2e610af82
         {
             float xMoveDist = speedH * Input.GetAxis("Mouse X"), yMoveDist = speedV * Input.GetAxis("Mouse Y");
             
@@ -60,14 +79,31 @@ public class PlayerController : MonoBehaviour {
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, minDist))
+        if (Physics.Raycast(ray, out hit, minDist))
         {
-            if(hit.collider.tag == "Interactable")
+            if (hit.collider.tag == "Interactable")
             {
-                print("SUPER");
-                //Interactable interactable = hit.collider.GetComponent<Interactable>();
+                //print("SUPER");
+                interactable = hit.collider.GetComponent<Interactable>();
+                interactable.setTexture(inventorySlots[0]);
+                interactable.popOut();
                 interactableName.text = hit.collider.name;
             }
-        } 
+        }
+        else
+        {
+            interactableName.text = " ";
+        }
+    }
+
+    void tempSpawn()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if(interactable != null)
+            {
+                interactable.spawn(inventorySlots[0], transform.position, transform.rotation);
+            }
+        }
     }
 }
