32476b40b9282e36aca10388288bf9ad6988c9f4
ATchibo <44547421+ATchibo@users.noreply.github.com>
2019-01-31 18:15:57 +0200

    dadaddadaaaaaa

--- EscapeRoom-2/EscapeRoom/Assets/Scripts/LightSwitch.cs
+++ EscapeRoom-2/EscapeRoom/Assets/Scripts/LightSwitch.cs
@@ -7,59 +7,37 @@ public class LightSwitch : MonoBehaviour
     public GameObject switcher;
     public GameObject[] lights;
 
-    public static bool isQualityLow;
-    public bool entered = false;
+    public static bool isQualityLow, set;
 
     private void Start()
     {
-        if (PlayerPrefs.GetInt("QualityLevel") == 0)
-            isQualityLow = true;
-        else isQualityLow = false;
-
-        if (isQualityLow || !entered)
-            turnOnAndOffTheLights(false);
-        else turnOnAndOffTheLights(true);
+        turnOnOrOffTheLights(false);
     }
 
     private void Update()
     {
-        if (isQualityLow)
-        {
-            print("Le sting");
-            turnOnAndOffTheLights(false);
-        } else if(!isQualityLow)
-        {
-            print("Le aprind");
-            if(entered)
-                turnOnAndOffTheLights(true);
-        }
+        if (!switcher.GetComponent<BoxCollider>().enabled)
+            turnOnOrOffTheLights(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
-            print("A intrat/iesit playerul " + entered);
-            entered = lights[0].transform.GetChild(0).gameObject.active;
-            if (!entered)
-            {
-                if (!isQualityLow)
-                    turnOnAndOffTheLights(true);
-                entered = true;
-            }
-            else
-            {
-                if (!isQualityLow)
-                    turnOnAndOffTheLights(false);
-                entered = false;
-            }
+            turnOnOrOffTheLights(true);
         }
     }
 
-    private void turnOnAndOffTheLights(bool state)
+    private void OnTriggerExit(Collider other)
     {
-        if (lights[0].transform.GetChild(0).gameObject.active == state)
-            return;
+        if (other.gameObject.tag == "Player")
+        {
+            turnOnOrOffTheLights(false);
+        }
+    }
+
+    private void turnOnOrOffTheLights(bool state)
+    {
         for (int i = 0; i < lights.Length; ++i)
         {
             lights[i].transform.GetChild(0).gameObject.SetActive(state);
--- EscapeRoom-2/EscapeRoom/Assets/Scripts/LightSwitchHandler.cs
+++ EscapeRoom-2/EscapeRoom/Assets/Scripts/LightSwitchHandler.cs
@@ -23,16 +23,27 @@ public class LightSwitchHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isQualityLow && !set)
+        if (isQualityLow && !set)
         {
             set = true;
             mainLight.SetActive(true);
-            LightSwitch.isQualityLow = true;
-        } else if(!isQualityLow && set)
+
+            for (int i = 0; i < lightSwitchers.Length; ++i)
+            {
+                print("Sting " + lightSwitchers[i]);
+                lightSwitchers[i].gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+        else if (!isQualityLow && set)
         {
             set = false;
             mainLight.SetActive(false);
-            LightSwitch.isQualityLow = false;
+
+            for (int i = 0; i < lightSwitchers.Length; ++i)
+            {
+                print("Aprind " + lightSwitchers[i]);
+                lightSwitchers[i].gameObject.GetComponent<Collider>().enabled = true;
+            }
         }
     }
 }
