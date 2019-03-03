e1c55df2593ea43b46b4b57a481b5898b2608b15
ATchibo <44547421+ATchibo@users.noreply.github.com>
2019-03-03 15:30:16 +0200

    ce bine ma simt in adidasii mei

---
+++ EscapeRoom-2/EscapeRoom/Assets/Scripts/PC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+    Animator animator;
+
+    bool started;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        started = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.enabled)
+        {
+
+        }
+    }
+}
